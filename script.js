131e4ef978dca24eddc353682fe9870f41d9bd34
unknown <tobias.leite14@gmail.com>
2026-02-22 18:04:30 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // Maneira mais verbosa
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "assets/Avatar-Light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Tobias Amorim sorrindo, usando camisa preta, barba, de oculos escuros e fundo em gradiente horizontal — do roxo médio à esquerda para azul claro à direita",
+    )
+  } else {
+    img.setAttribute("src", "assets/Avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Tobias Amorim sorrindo, usando camisa preta, barba e fundo amarelo",
+    )
+  }
+}
