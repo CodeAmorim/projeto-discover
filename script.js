281a2fe4eb10abd2b71b1c84ce4939dfd80e654b
unknown <tobias.leite14@gmail.com>
2026-02-23 11:22:37 -0300

    light-mode photo corrected

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/Avatar-Light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Foto de Tobias Amorim sorrindo, usando camisa preta, barba, de oculos escuros e fundo em gradiente horizontal — do roxo médio à esquerda para azul claro à direita",
     )
   } else {
-    img.setAttribute("src", "assets/Avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
     img.setAttribute(
       "alt",
       "Foto de Tobias Amorim sorrindo, usando camisa preta, barba e fundo amarelo",
